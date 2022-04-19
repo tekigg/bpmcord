8de3430dbb83655397defe5530b3af9fb871aff5
iiiklox <iiiklox@gmail.com>
2022-04-19 20:28:54 +0300

    server fix

--- bpmcord/server.py
+++ bpmcord/server.py
@@ -1,13 +1,13 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 
-from .utils import exceptions, Log
+from utils import exceptions, Log
 
 class SimpleEcho(WebSocket):
     def handleMessage(self):
         Log(1, self.data)
     
     def handleConnected(self):
-        Log(1, self.data)
+        Log(1, "Client connected successfully.")
 
     def handleClose(self):
         Log(3, "Client disconnected from the server.")
